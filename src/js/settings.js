ccdbb37ccb454c0b2546c653c47817399e11be8a
steffibe <steffibe@umich.edu>
2023-11-15 11:03:52 -0500

    changed font size options to allow for larger font sizes

--- src/js/settings.js
+++ src/js/settings.js
@@ -150,12 +150,12 @@ const Settings = ({ updateLoginStatus }) => {
               value={fontSize}
               onChange={(e) => setFontSize(e.target.value)}
             >
-              <option value="14px">14px</option>
-              <option value="16px">16px</option>
+              <option value="12px">12px</option>
               <option value="18px">18px</option>
-              <option value="20px">20px</option>
-              <option value="22px">22px</option>
               <option value="24px">24px</option>
+              <option value="30px">30px</option>
+              <option value="36px">36px</option>
+              <option value="48px">48px</option>
             </select>
             <label className="tooltip">
               Translation Font Color
